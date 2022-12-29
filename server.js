6a2d5e3ac4e899f5a0eff8d639db17cfc061772c
Akash praveen kumar R <98536933+Akashakke@users.noreply.github.com>
2022-12-29 10:16:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({origin:"https://webcode-guvi.onrender.com"}));
+app.use(cors());
 app.get('/api/keys/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
